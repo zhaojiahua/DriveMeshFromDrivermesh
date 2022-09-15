2ab326574080020dcf28668f42347edefff00bc4
zhaojiahua <53359717+zhaojiahua@users.noreply.github.com>
2022-09-15 19:07:29 +0800

    Add files via upload

--- Project1/CVTest.cpp
+++ Project1/CVTest.cpp
@@ -2,6 +2,7 @@
 #include "CVTest.h"
 #include "cmath"
 
+
 using namespace cv;
 using namespace std;
 
@@ -18,25 +19,39 @@ int main()
 	//{
 	//	cout << (int)img_bytes[i] << " ";
 	//}
+	vector<vector<double>> Point_diffs;
+	LoadDiffData("D:/UE5_Project/TTSAPro/AssetsFromOther/axPatDr50.txt", Point_diffs);
 	
+	CreateTextTo(Point_diffs, 1024, "writetestpng3.png");
 	
-	//CreateTextTo(1024, "writetestpng3.png");
-	//提取驱动模型Dr的obj信息
-	vector<vector<double>> vertexs_Dr;
-	vector<vector<double>> normals_Dr;
-	vector<vector<double>> uvs_Dr;
-	vector<vector<int>> faces_vertexs_Dr;
-	vector<vector<int>> faces_uvs_Dr;
-	LoadObj("D:/UE5_Project/TTSAPro/AssetsFromOther/Dr.obj", vertexs_Dr, normals_Dr, uvs_Dr, faces_vertexs_Dr, faces_uvs_Dr);
-	//提取爱夏模型的obj信息
-	vector<vector<double>> vertexs_Ax;
-	vector<vector<double>> normals_Ax;
-	vector<vector<double>> uvs_Ax;
-	vector<vector<int>> faces_vertexs_Ax;
-	vector<vector<int>> faces_uvs_Ax;
-	LoadObj("D:/UE5_Project/TTSAPro/AssetsFromOther/aixia.obj", vertexs_Ax, normals_Ax, uvs_Ax, faces_vertexs_Ax, faces_uvs_Ax);
+	////提取驱动模型Dr的obj信息
+	//vector<vector<double>> vertexs_Dr = {};
+	//vector<vector<double>> normals_Dr = {};
+	//vector<vector<double>> uvs_Dr = {};
+	//vector<vector<int>> faces_vertexs_Dr = {};
+	//vector<vector<int>> faces_uvs_Dr = {};
+	//LoadObj("D:/UE5_Project/TTSAPro/AssetsFromOther/Dr.obj", vertexs_Dr, normals_Dr, uvs_Dr, faces_vertexs_Dr, faces_uvs_Dr);
+	////提取爱夏模型的obj信息
+	//vector<vector<double>> vertexs_Ax = {};
+	//vector<vector<double>> normals_Ax = {};
+	//vector<vector<double>> uvs_Ax = {};
+	//vector<vector<int>> faces_vertexs_Ax = {};
+	//vector<vector<int>> faces_uvs_Ax = {};
+	//LoadObj("D:/UE5_Project/TTSAPro/AssetsFromOther/aixia.obj", vertexs_Ax, normals_Ax, uvs_Ax, faces_vertexs_Ax, faces_uvs_Ax);
 
-	
+	////计算所有的UV对应的空间坐标
+	//vector<vector<double>> AxPointAtDr = {};
+	//for (auto i= uvs_Ax.begin();i != uvs_Ax.end();i++)
+	//{
+	//	vector<double> tempResult = CalculateInterpolation(vertexs_Dr, uvs_Dr, faces_vertexs_Dr, faces_uvs_Dr, *i);
+	//	AxPointAtDr.push_back(tempResult);
+	//}
+	//cout << AxPointAtDr.size() << endl;
+
+	//vector<double> testUV = { 0.02024, 0.02105 };
+	//vector<double>testResult = CalculateInterpolation(vertexs_Dr, uvs_Dr, faces_vertexs_Dr, faces_uvs_Dr, testUV);
+	//cout << testResult[0] << " " << testResult[1] << " " << testResult[2] << " " << testResult[3] << endl;
+
 	return 0;
 }
 
@@ -48,29 +63,36 @@ uint8_t* MatToBytes(Mat image)
 	return bytes;
 };
 
-void createAlphaMat(Mat& mat)
+void createAlphaMat(vector<vector<double>> inPoint_diffs,Mat& mat)
 {
+	//设置底色为纯黑
 	for (int i = 0; i < mat.rows; ++i)
 	{
 		for (int j = 0; j < mat.cols; ++j)
 		{
 			Vec4b& rgba = mat.at<Vec4b>(i, j);
-			//rgba[0] = UCHAR_MAX;
-			//rgba[1] = saturate_cast<uchar>((float(mat.cols - j)) / ((float)mat.cols) * UCHAR_MAX);
-			//rgba[2] = saturate_cast<uchar>((float(mat.cols - i)) / ((float)mat.rows) * UCHAR_MAX);
-			//rgba[3] = rgba[1] = saturate_cast<uchar>(rgba[1] + rgba[2]);
 			rgba[0] = 0;
 			rgba[1] = 0;
-			rgba[2] = (i / 1024.0) * 255;
+			rgba[2] = 0;
 			rgba[3] = 255;
 		}
 	}
+	//上色
+	for (int i = 0; i < inPoint_diffs.size(); i++)
+	{
+		int pixelX = (1 - inPoint_diffs[i][1]) * mat.rows;
+		int pixelY = inPoint_diffs[i][0] * mat.cols;
+		Vec4b& rgba = mat.at<Vec4b>(pixelX, pixelY);
+		rgba[0] = inPoint_diffs[i][2];
+		rgba[1] = inPoint_diffs[i][3];
+		rgba[2] = inPoint_diffs[i][4];
+	}
 }
 
-void CreateTextTo(int insize, string infilename)
+void CreateTextTo(vector<vector<double>> inPoint_diffs, int insize, string infilename)
 {
 	Mat mat(insize, insize, CV_8UC4);
-	createAlphaMat(mat);
+	createAlphaMat(inPoint_diffs,mat);
 	vector<int>compression_params;
 	compression_params.push_back(IMWRITE_PNG_COMPRESSION);
 	compression_params.push_back(9);
@@ -127,12 +149,12 @@ void LoadObj(string inpath, vector<vector<double>>& vectexs, vector<vector<doubl
 			else if (sline[0] == 'f')
 			{
 				istringstream ins(sline);
-				vector<int> tempFaceVertexIndex(3);
-				vector<int> tempFaceUVs(3);
+				vector<int> tempFaceVertexIndex(4);
+				vector<int> tempFaceUVs(4);
 				//读取顶点和uv索引
 				ins >> s0;//首先把f去掉
 				string s1;
-				for (int i = 0; i < 3; i++)
+				for (int i = 0; i < 4; i++)	//有的模型是四点面,三点面的模型直接截断掉第四个值就行了
 				{
 					ins >> s1;
 					int a = 0, k;
@@ -157,20 +179,76 @@ void LoadObj(string inpath, vector<vector<double>>& vectexs, vector<vector<doubl
 	}
 }
 
-vector<double> CalculateInterpolation(vector<vector<double>> inuvs, vector<vector<int>> inface_uvindex, vector<double> inUV)
+vector<double> CalculateInterpolation(vector<vector<double>> invertexs,vector<vector<double>> inuvs, vector<vector<int>> inface_vertindex, vector<vector<int>> inface_uvindex, vector<double> inUV)
 {
+	vector<double> result = { 0,0,0 };
 	for (int i = 0; i < inface_uvindex.size(); i++)
 	{
 		int uvindex0 = inface_uvindex[i][0];
 		int uvindex1 = inface_uvindex[i][1];
 		int uvindex2 = inface_uvindex[i][2];
-		vector<double> uv0 = inuvs[uvindex0];
-		vector<double> uv1 = inuvs[uvindex1];
-		vector<double> uv2 = inuvs[uvindex2];
+		int vertindex0 = inface_vertindex[i][0];
+		int vertindex1 = inface_vertindex[i][1];
+		int vertindex2 = inface_vertindex[i][2];
+		vector<double> uv0 = inuvs[uvindex0 - 1];	//注意f面提供的序号都是从1开始的
+		vector<double> uv1 = inuvs[uvindex1 - 1];
+		vector<double> uv2 = inuvs[uvindex2 - 1];
+		vector<double> P0 = invertexs[vertindex0 - 1];
+		vector<double> P1 = invertexs[vertindex1 - 1];
+		vector<double> P2 = invertexs[vertindex2 - 1];
 
-
+		vector<double> TriRatio = CalculateAreaRatio(uv0, uv1, uv2, inUV);
+		if ((TriRatio[0] + TriRatio[1] + TriRatio[2]) <= 1)	//三个比值之和小于1才说明点在三角形内部,然后返回三个插值和面序号
+		{
+			//直接返回世界坐标(插值也可以顺带返回出去,以便后期计算其他的信息使用)
+			result[0] = P0[0] * TriRatio[0] + P1[0] * TriRatio[1] + P2[0] * TriRatio[2];
+			result[1] = P0[1] * TriRatio[0] + P1[1] * TriRatio[1] + P2[1] * TriRatio[2];
+			result[2] = P0[2] * TriRatio[0] + P1[2] * TriRatio[1] + P2[2] * TriRatio[2];
+			//result[3] = i;
+			//result[4] = TriRatio[0];
+			//result[5] = TriRatio[1];
+			//result[6] = TriRatio[2];
+			return result;
+		}
 	}
-	return vector<double>();
+	return result;
+}
+
+vector<double> CalculateAreaRatio(vector<double> uv0, vector<double> uv1, vector<double> uv2, vector<double> inUV)
+{
+	vector<double> result = { 0,0,0 };
+	double S = 0.5 * abs((uv1[0] - uv0[0]) * (uv2[1] - uv0[1]) - (uv1[1] - uv0[1]) * (uv2[0] - uv0[0]));//三角形总面积
+	double s0 = 0.5*abs((uv1[0] - inUV[0]) * (uv2[1] - inUV[1]) - (uv1[1] - inUV[1]) * (uv2[0] - inUV[0]));//与uv0对应的三角形面积
+	double s1 = 0.5 * abs((inUV[0] - uv0[0]) * (uv2[1] - uv0[1]) - (inUV[1] - uv0[1]) * (uv2[0] - uv0[0]));//与uv1对应的三角形面积
+	double s2 = 0.5 * abs((uv1[0] - uv0[0]) * (inUV[1] - uv0[1]) - (uv1[1] - uv0[1]) * (inUV[0] - uv0[0]));//与uv2对应的三角形面积
+	result[0] = s0 / S;
+	result[1] = s1 / S;
+	result[2] = s2 / S;
+	return result;
+}
+
+void LoadDiffData(string inpath, vector<vector<double>>& Point_diffs)
+{
+	ifstream infile;
+	infile.open(inpath);
+	if (!infile.is_open())
+	{
+		cout << "读取文件失败!" << endl;
+	}
+	else
+	{
+		cout << "开始加载数据" << endl;
+		string sline;
+		while (getline(infile, sline))
+		{
+			vector<double> tempVector(5);
+			istringstream ins(sline);
+			ins >> tempVector[0] >> tempVector[1] >> tempVector[2] >> tempVector[3] >> tempVector[4];
+			Point_diffs.push_back(tempVector);
+		}
+		infile.close();
+		cout << "加载完毕" << endl;
+	}
 }
 
 
--- Project1/CVTest.h
+++ Project1/CVTest.h
@@ -14,10 +14,14 @@ using namespace std;
 
 uint8_t* MatToBytes(class Mat image);
 
-void createAlphaMat(class Mat& mat);
+void createAlphaMat(vector<vector<double>> inPoint_diffs, class Mat& mat);
 
-void CreateTextTo(int insize, string infilename);
+void CreateTextTo(vector<vector<double>> inPoint_diffs, int insize, string infilename);
 
 void LoadObj(string inpath, vector<vector<double>>& vectexs, vector<vector<double>>& normals, vector<vector<double>>& uvs, vector<vector<int>>& faces_vertexs, vector<vector<int>>& faces_uvs);
 
-vector<double> CalculateInterpolation(vector<vector<double>> inuvs,vector<vector<int>> inface_uvindex, vector<double> inUV);//传一个uv值,然后判断这个uv是落在哪个面上,查找到之后顺便把插值计算出来
+vector<double> CalculateInterpolation(vector<vector<double>> invertexs, vector<vector<double>> inuvs, vector<vector<int>> inface_vertindex, vector<vector<int>> inface_uvindex, vector<double> inUV);//传一个uv值,然后判断这个uv是落在哪个面上,查找到之后顺便把插值计算出来
+
+vector<double> CalculateAreaRatio(vector<double> uv0, vector<double> uv1, vector<double> uv2, vector<double> inUV);
+
+void LoadDiffData(string inpath, vector<vector<double>>& Point_diffs);
